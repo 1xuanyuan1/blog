ea755e9ca2ce897cfc6ac37fe95a428c02b28645
Duke <546407233@qq.com>
2017-03-15 11:05:31 +0800

    增加webpack-bundle-analyzer插件来分析webpack打包情况

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const utils = require('./utils')
 const base = require('./webpack.base.config')
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = merge(base, {
   target: 'node',
   devtool: false,
@@ -19,6 +19,8 @@ module.exports = merge(base, {
   },
   externals: Object.keys(require('../package.json').dependencies),
   plugins: [
+    // webpack-bundle-analyzer 来分析 Webpack 生成的包体组成并且以可视化的方式反馈给开发者
+    new BundleAnalyzerPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"server"'
